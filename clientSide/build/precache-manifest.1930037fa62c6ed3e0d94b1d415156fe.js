82db5083eb162169f5403b6a4b193be028d73942
barakformanski <barakformanski@gmail.com>
2020-10-16 11:25:24 +0300

    npm run build again

---
+++ clientSide/build/precache-manifest.1930037fa62c6ed3e0d94b1d415156fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f17e633faa6b3857b29d50e9a3c47ffb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16eb21a1c9abc947dea2",
+    "url": "/static/css/2.e10332d9.chunk.css"
+  },
+  {
+    "revision": "26ac5eb20c7fc6522e14",
+    "url": "/static/css/main.a9266c6e.chunk.css"
+  },
+  {
+    "revision": "16eb21a1c9abc947dea2",
+    "url": "/static/js/2.23893d6d.chunk.js"
+  },
+  {
+    "revision": "c69fcf8670b313a5daf5b087e062c182",
+    "url": "/static/js/2.23893d6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26ac5eb20c7fc6522e14",
+    "url": "/static/js/main.3e8045d5.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
